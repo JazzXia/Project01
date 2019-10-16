b64cbfc971f323ea34f86378bfe6fbba8783d444
JazzXia <1104841692@qq.com>
2019-10-16 15:07:45 +0800

    Maven测试

---
+++ mavendemo/src/main/java/com/qtatelier/maven/mavendemo/MavendemoApplication.java
@@ -0,0 +1,13 @@
+package com.qtatelier.maven.mavendemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavendemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavendemoApplication.class, args);
+    }
+
+}
